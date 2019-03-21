f40a3b698d298f17547c38a972f6be5570382d46
Chelsea <chelsea.doeleman@hva.nl>
2019-03-21 16:49:22 +0100

    remove herokucorsanywhere

--- server/js/helpers/getUnsplashUrl.js
+++ server/js/helpers/getUnsplashUrl.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const getUnsplashUrl = (pageNumber) =>  {
-    const baseUrl = 'https://cors-anywhere.herokuapp.com/https://api.unsplash.com/photos/?client_id='
+    const baseUrl = 'https://api.unsplash.com/photos/?client_id='
     const unsplash = 'd633cd88d3ab4d70df0bfa48b64ee1241d0d56f25c16a78e451f20172dbda585'
     const addPage = `&page=${pageNumber}`
     const resultsPage = '&per_page=30'
@@ -9,7 +9,7 @@ const getUnsplashUrl = (pageNumber) =>  {
 }
 
 const getUnsplashDetail = (id) =>  {
-    const baseUrl = `https://cors-anywhere.herokuapp.com/https://api.unsplash.com/photos/${id}?client_id=`
+    const baseUrl = `https://api.unsplash.com/photos/${id}?client_id=`
     const unsplash = 'd633cd88d3ab4d70df0bfa48b64ee1241d0d56f25c16a78e451f20172dbda585'
     return `${baseUrl}${unsplash}`
 }
